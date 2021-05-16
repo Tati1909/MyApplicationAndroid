2c1f3b72545a206a4713037123c277ee36556592
татьяна <emuart155@gmail.com>
2021-05-16 21:12:26 +0300

    Сделала ДЗ 1.

--- app/src/main/java/com/example/myapplicationandroid/MainActivity.java
+++ app/src/main/java/com/example/myapplicationandroid/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.myapplicationandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonTykTyk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonTykTyk = findViewById(R.id.button1);
+        buttonTykTyk.setOnClickListener(v -> buttonTykTyk.setText("ФАЗАН"));
     }
+
 }
