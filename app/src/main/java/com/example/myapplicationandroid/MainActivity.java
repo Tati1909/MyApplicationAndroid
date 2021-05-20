016d002ee9d66e0aad2d0b1a0c96e79ff6c8265d
татьяна <emuart155@gmail.com>
2021-05-20 19:32:18 +0300

    Добавила ScrollView

--- app/src/main/java/com/example/myapplicationandroid/MainActivity.java
+++ app/src/main/java/com/example/myapplicationandroid/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.myapplicationandroid;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonTykTyk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonTykTyk = findViewById(R.id.button1);
+        buttonTykTyk.setOnClickListener(v -> buttonTykTyk.setText("ФАЗАН"));
     }
+
+
 }
